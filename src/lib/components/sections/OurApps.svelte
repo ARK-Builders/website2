2e22bc1b8c2b018ac95e16283ada7d49b092344b
prokawsar <prokawsar@gmail.com>
2024-06-27 19:10:31 +0600

    Dynamic bg colors

--- src/lib/components/sections/OurApps.svelte
+++ src/lib/components/sections/OurApps.svelte
@@ -13,11 +13,13 @@
 
 		<div class="flex w-full flex-col gap-7 lg:flex-row">
 			<!-- Left side panel -->
-			<div class="bg-arkGray6 flex w-1/5 flex-row lg:flex-col">
+			<div class="flex w-1/5 flex-row overflow-hidden bg-arkGray6 lg:flex-col">
 				{#each appList as app}
 					<button
+						style="background-color: {activeApp.name == app.name ? app.colors[1] : ''};"
 						on:click={() => (activeApp = app)}
-						class="flex w-full flex-row items-center gap-3 border-b bg-transparent px-5 py-3 font-semibold lg:h-[88px]"
+						class="
+							flex w-full flex-row items-center gap-3 border-b bg-transparent px-5 py-3 font-semibold first:rounded-tl-lg first:rounded-tr-lg last:rounded-bl-lg last:rounded-br-lg lg:h-[88px]"
 					>
 						<img src="/images/app-logos/{app.logo || 'placeholder'}.png" alt="app logo" />
 						{app.name}
@@ -26,7 +28,10 @@
 			</div>
 
 			<!-- App details -->
-			<div class="bg-arkOrangeLighter flex w-4/5 flex-col justify-between rounded-md p-5">
+			<div
+				style="background-color: {activeApp.colors[0]};"
+				class="flex w-4/5 flex-col justify-between rounded-md p-5"
+			>
 				<div class="flex flex-col gap-3">
 					<p class="text-3xl font-bold">{activeApp.name}</p>
 					<p>{activeApp.description}</p>
--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -4,36 +4,42 @@ export const appList = [
 		description:
 			'Navigator is a file browser that embraces tags as the novel method to locate your data. It effectively eliminates irrelevant system files, enabling you to find your documents or photos with ease through the use of tags.',
 		logo: 'memo',
-		url: ''
+		url: '',
+		colors: ['#FFF5DD', '#FFD979'] // 1st lighter, 2nd deeper
 	},
 	{
 		name: 'Navigator',
 		description: '',
 		logo: 'navigator',
-		url: ''
+		url: '',
+		colors: ['#A5D6FF', '#60B1F4']
 	},
 	{
 		name: 'Shelf',
 		description: '',
 		logo: 'shelf',
-		url: ''
+		url: '',
+		colors: ['#A5D6FF', '#60B1F4']
 	},
 	{
 		name: 'Rate',
 		description: '',
 		logo: 'rate',
-		url: ''
+		url: '',
+		colors: ['#E1D0F7', '#C39EF2']
 	},
 	{
 		name: 'Retouch',
 		description: '',
 		logo: 'retouch',
-		url: ''
+		url: '',
+		colors: ['#DBF5C8', '#9EE67D']
 	},
 	{
 		name: 'Gallery',
 		description: '',
 		logo: '',
-		url: ''
+		url: '',
+		colors: ['#A5D6FF', '#60B1F4']
 	}
 ]
