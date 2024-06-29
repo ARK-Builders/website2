5c6123cb46b84c1c4044d12d230e98d200e2e26e
prokawsar <prokawsar@gmail.com>
2024-06-29 08:25:44 +0600

    hero fix

--- src/lib/components/sections/Hero.svelte
+++ src/lib/components/sections/Hero.svelte
@@ -4,19 +4,19 @@
 </script>
 
 <div class="mb-16 flex w-full flex-col">
-	<div class="relative flex w-full flex-row items-center justify-between gap-1">
+	<div class="relative mb-10 flex w-full flex-row items-center justify-between gap-1 py-10">
 		<div class="flex-1">
 			<img class="hidden md:block" src="{base}/images/left.png" alt="left" />
 			<img class="sm:hidden" src="{base}/images/mob-left.png" alt="left" />
 		</div>
 
-		<div class="absolute flex max-w-2xl flex-col gap-5 md:relative md:gap-10">
+		<div class="absolute flex max-w-2xl flex-col gap-5 md:relative md:gap-14">
 			<div class="flex flex-col items-center gap-2 md:gap-5">
 				<p class="text-center text-3xl font-semibold md:text-[40px] lg:text-[66px]">
 					Neat, efficient and
 				</p>
 				<p
-					class="w-fit rounded-lg bg-arkOrange px-4 text-center text-2xl font-semibold text-white md:text-4xl md:leading-tight lg:text-[66px]"
+					class="w-fit rounded-lg bg-arkOrange px-4 text-center text-2xl font-semibold text-white md:text-4xl md:leading-snug lg:text-[66px]"
 				>
 					privacy-focused apps
 				</p>
