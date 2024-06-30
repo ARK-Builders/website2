0244b196bae6125137ae5f449301d9884fc1538c
prokawsar <prokawsar@gmail.com>
2024-06-30 09:25:38 +0600

    Error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import { page } from '$app/stores'
+</script>
+
+<svelte:head>
+	<title>ARK Builders | {$page.error?.message}</title>
+</svelte:head>
+
+<div class="mx-auto flex max-w-7xl flex-col justify-center gap-3">
+	<h1 class="text-center text-6xl" style="color: red;">ERROR</h1>
+	<p class="text-center" style="color: red;">{$page.error?.message}</p>
+</div>
