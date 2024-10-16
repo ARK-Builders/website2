1f8236297c94734676fa0b42ded9b83f7845e0a5
Kawsar Ahmed <prokawsar@gmail.com>
2024-10-16 12:19:56 +0600

    Blog cleanup

--- src/lib/components/elements/BlogItem.svelte
+++ src/lib/components/elements/BlogItem.svelte
@@ -1,9 +1,8 @@
 <script lang="ts">
 	import { base } from '$app/paths'
 	import type { Blog } from '$utils/constants'
-	export let post: Blog
 
-	console.log(post)
+	export let post: Blog
 </script>
 
 <div class="flex flex-col gap-3 rounded-xl bg-arkGray5 p-5">
@@ -11,7 +10,6 @@
 		<p class="text-2xl font-extrabold">{post.title}</p>
 		<p class="font-medium text-arkGray4">{post.date}</p>
 	</div>
-	<!-- <img src="https://placehold.co/330x185" alt="placeholder" /> -->
 	<p>
 		{post.summary}
 	</p>
