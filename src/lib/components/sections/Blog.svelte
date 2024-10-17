fdd870a32cc8c6b8c9f26d7b1809e5c66a7c0730
Kawsar Ahmed <prokawsar@gmail.com>
2024-10-17 06:46:18 +0600

    Fixed blog size

--- src/lib/components/sections/Blog.svelte
+++ src/lib/components/sections/Blog.svelte
@@ -13,7 +13,7 @@
 		</div>
 
 		<div class="grid grid-cols-1 gap-5 sm:grid-cols-2 lg:grid-cols-3">
-			{#each posts as post}
+			{#each posts.slice(0, 3) as post}
 				<BlogItem {post} />
 			{/each}
 		</div>
