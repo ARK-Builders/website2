061b89c7cbc790461201fd1cb143b6adc27614e9
Kawsar Ahmed <prokawsar@gmail.com>
2024-10-17 11:00:07 +0600

    Rendering json instead of .mp4 video

---
+++ src/lib/components/elements/LottieAnimation.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+	import { onMount } from 'svelte'
+	import lottie from 'lottie-web'
+
+	export let src = ''
+	export let width = '100%'
+	export let height = '100%'
+
+	let container: HTMLDivElement
+
+	onMount(() => {
+		const animation = lottie.loadAnimation({
+			container: container,
+			renderer: 'svg',
+			loop: true,
+			autoplay: true,
+			path: src
+		})
+
+		return () => animation.destroy()
+	})
+</script>
+
+<div bind:this={container} style="width: {width}; height: {height};"></div>
