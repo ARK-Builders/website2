8c1cbf0dc70cda39b9dd96ad48febbea97da4082
Kawsar Ahmed <prokawsar@gmail.com>
2024-11-01 09:33:35 +0600

    Indexing pull request for contribute

--- content/issues/fetchIssues.js
+++ content/issues/fetchIssues.js
@@ -1,7 +1,8 @@
 import axios from 'axios'
 
 async function callApi() {
-	var issuesList = []
+	let issuesList = []
+	let pullRequestList = []
 	try {
 		var repositories = await axios.get(
 			'https://raw.githubusercontent.com/ARK-Builders/website2/main/content/issues/repos-list.json'
@@ -13,6 +14,30 @@ async function callApi() {
 				const issues = await axios.get(
 					'https://api.github.com/repos/ARK-Builders/' + repo.name + '/issues'
 				)
+				const pullRequests = await axios.get(
+					'https://api.github.com/repos/ARK-Builders/' + repo.name + '/pulls'
+				)
+
+				if (pullRequests.status === 200) {
+					if (pullRequests.data.length > 0) {
+						for (const pulls of pullRequests.data) {
+							let repoUrl = pulls.url.replace('api.', '').replace('repos/', '')
+							if (pulls.user.login == 'dependabot[bot]') continue
+
+							pullRequestList.push({
+								title: pulls.title,
+								labels: ['pull request'],
+								user: pulls.user.login,
+								user_avatar: pulls.user.avatar_url,
+								date: new Date(pulls.created_at),
+								repo: repoUrl,
+								number: pulls.number,
+								platforms: repo.platforms,
+								languages: repo.languages
+							})
+						}
+					}
+				}
 
 				if (issues.status === 200) {
 					if (issues.data.length > 0) {
@@ -45,7 +70,7 @@ async function callApi() {
 					throw new Error(`API request failed with status ${response.status}`)
 				}
 			}
-			return issuesList
+			return [...issuesList, ...pullRequestList]
 		} else {
 			throw new Error(`API request failed with status ${response.status}`)
 		}
