71ea21d647b0bf2ba1e8010eac25a3b8e2446a4a
Kawsar Ahmed <prokawsar@gmail.com>
2024-11-01 09:54:08 +0600

    Fix spaces and bg color

--- src/lib/components/layouts/Header.svelte
+++ src/lib/components/layouts/Header.svelte
@@ -13,7 +13,7 @@
 </script>
 
 <header class="sticky top-0 z-10 bg-arkGray bg-opacity-95 px-5 dark:bg-gray-900 xl:px-0">
-	<div class="mx-auto flex h-[72px] items-center justify-between sm:px-8 lg:px-0 xl:max-w-7xl">
+	<div class="mx-auto flex h-[72px] items-center justify-between lg:px-0 xl:max-w-7xl">
 		<div class="flex flex-row items-center">
 			<a href="{base}/" aria-label={config.headerTitle}>
 				<div class="flex items-center justify-between">
