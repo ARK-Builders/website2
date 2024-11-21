8e014585d17bba7001d9c7c1e940e539738d22cf
Kawsar Ahmed <prokawsar@gmail.com>
2024-11-21 10:20:52 +0600

    Apps list updated with current app screenshot

--- src/lib/components/elements/AppsCard.svelte
+++ src/lib/components/elements/AppsCard.svelte
@@ -1,9 +1,18 @@
 <script lang="ts">
 	import { base } from '$app/paths'
 	import MemoImage1 from '$lib/assets/images/apps/memo/1.png'
+	import RateImage1 from '$lib/assets/images/apps/rate/1.png'
 	import { appLogos, type App } from '$utils/constants'
 
 	export let app: App
+
+	const appImages: Record<string, string> = {
+		rate1: RateImage1,
+		memo1: MemoImage1
+	}
+
+	const getImagesWithName = (name: string) =>
+		Object.entries(appImages).filter((value) => value[0].includes(name))[0]
 </script>
 
 <div class="flex flex-col gap-3 rounded-xl bg-arkGray5 p-5">
@@ -20,8 +29,8 @@
 	</div>
 
 	<img
-		class="max-h-[220px] rounded-md object-cover lg:max-h-[350px]"
-		src={MemoImage1}
+		class="max-h-[220px] rounded-md object-cover object-top lg:max-h-[350px]"
+		src={getImagesWithName(app.title.toLowerCase())[1]}
 		alt="{app.title} app screens"
 	/>
 
