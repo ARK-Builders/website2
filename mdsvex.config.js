0c77ecc5de4ed79b9a5c408a5c6e20281f66ee2b
Kawsar Ahmed <prokawsar@gmail.com>
2024-11-26 13:32:25 +0600

    Fix markdown heading styles

--- mdsvex.config.js
+++ mdsvex.config.js
@@ -1,12 +1,14 @@
 // import remarkPlugins from './src/plugins/remark/index.js';
-import rehypePlugins from './src/plugins/rehype/index.js'
+import relativeImages from 'mdsvex-relative-images'
 import rehypeExternalLinks from 'rehype-external-links'
+import rehypePlugins from './src/plugins/rehype/index.js'
 
 export default {
 	extensions: ['.md'],
 	smartypants: {
 		dashes: 'oldschool'
 	},
+	remarkPlugins: [relativeImages],
 	// remarkPlugins: [...Object.values(remarkPlugins)],
 	rehypePlugins: [
 		[rehypeExternalLinks, { target: '_blank', rel: ['noopener', 'noreferrer'] }],
