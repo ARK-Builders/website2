92225143f847ac21366371bc11a93ec9c44c2648
Kawsar Ahmed <prokawsar@gmail.com>
2024-11-28 20:19:16 +0600

    Lottie import browser only

--- src/lib/components/elements/LottieAnimation.svelte
+++ src/lib/components/elements/LottieAnimation.svelte
@@ -1,23 +1,26 @@
 <script lang="ts">
+	import { browser } from '$app/environment'
+	import lottie, { type AnimationItem } from 'lottie-web'
 	import { onMount } from 'svelte'
-	import lottie from 'lottie-web'
 
 	export let src = ''
 	export let width = '100%'
 	export let height = '100%'
 
 	let container: HTMLDivElement
+	let animation: AnimationItem
 
 	onMount(() => {
-		const animation = lottie.loadAnimation({
-			container: container,
-			renderer: 'svg',
-			loop: true,
-			autoplay: true,
-			path: src
-		})
-
-		return () => animation.destroy()
+		if (browser) {
+			animation = lottie.loadAnimation({
+				container: container,
+				renderer: 'svg',
+				loop: true,
+				autoplay: true,
+				path: src
+			})
+		}
+		return () => animation?.destroy()
 	})
 </script>
 
