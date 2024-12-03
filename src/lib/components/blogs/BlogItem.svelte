ecf22199122309ce15d9dea7c43794b8f6a25cfb
Kawsar Ahmed <prokawsar@gmail.com>
2024-12-03 09:56:38 +0600

    Image for blog item

--- src/lib/components/blogs/BlogItem.svelte
+++ src/lib/components/blogs/BlogItem.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import { base } from '$app/paths'
+	import Image from '$lib/components/elements/Image.svelte'
 	import { config } from '$lib/config'
 	import type { Blog } from '$utils/constants'
 
@@ -11,19 +12,25 @@
 		href="{base}/blog/{post.slug}"
 		class="flex flex-col gap-3 rounded-xl bg-arkGray5 p-5 hover:bg-arkOrangeLight"
 	>
-		<div class="flex h-56 flex-col">
-			<time dateTime={post.date ?? post.date} class="text-arkGray4">
-				{new Date(post.date).toLocaleDateString(config.locale, {
-					year: 'numeric',
-					month: 'long',
-					day: 'numeric',
-				})}
-			</time>
-			<p class="text-2xl font-extrabold">{post.title}</p>
-			<div class="">
-				<p class="line-clamp-5">
-					{post.summary}
-				</p>
+		<div class="flex flex-col gap-2">
+			<Image
+				src="{base}{post.image ?? 'https://placehold.co/100x100/EEA050/white/?text=' + post.title}"
+				class="max-h-48 w-full rounded-md object-cover xl:max-h-52"
+			/>
+			<div class="flex h-52 flex-col gap-1">
+				<time dateTime={post.date ?? post.date} class="text-arkGray4">
+					{new Date(post.date).toLocaleDateString(config.locale, {
+						year: 'numeric',
+						month: 'long',
+						day: 'numeric',
+					})}
+				</time>
+				<p class="text-2xl font-extrabold">{post.title}</p>
+				<div class="">
+					<p class="line-clamp-5">
+						{post.summary}
+					</p>
+				</div>
 			</div>
 		</div>
 		<!-- <a href="{base}/blog/{post.slug}" class="w-fit font-bold text-arkOrange">Read</a> -->
