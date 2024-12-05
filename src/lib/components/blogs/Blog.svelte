a5e99bf64de124dd5774664969f710d28163e620
Kawsar Ahmed <prokawsar@gmail.com>
2024-12-05 22:11:53 +0600

    More blog post footer

--- src/lib/components/blogs/Blog.svelte
+++ src/lib/components/blogs/Blog.svelte
@@ -1,8 +1,9 @@
 <script>
 	import { base } from '$app/paths'
+	import Author from '$lib/components/Author.svelte'
+	import BlogItem from '$lib/components/blogs/BlogItem.svelte'
 	import RenderMarkdown from '$lib/components/RenderMarkdown.svelte'
 	import { config } from '$lib/config'
-	import Author from '../Author.svelte'
 
 	export let post
 	export let author
@@ -67,19 +68,6 @@
 				class="divide-y divide-gray-200 pb-8 dark:divide-gray-700 xl:grid xl:grid-cols-4 xl:gap-x-6 xl:divide-y-0"
 				style="grid-template-rows: auto 1fr;"
 			>
-				<!-- <dl class="py-6 xl:border-b xl:border-gray-200 xl:pt-10 xl:dark:border-gray-700">
-					<dt class="sr-only">Authors</dt>
-					<dd>
-						<ul
-							class="flex justify-center space-x-8 sm:space-x-12 xl:block xl:space-x-0 xl:space-y-8"
-						>
-							<li class="flex items-center space-x-2">
-								<Author author={author.name} avatar={author.avatar} twitter={author.twitter} />
-							</li>
-						</ul>
-					</dd>
-				</dl> -->
-
 				<div
 					class="divide-y divide-gray-200 dark:divide-gray-700 xl:col-span-4 xl:row-span-2 xl:pb-0"
 				>
@@ -96,41 +84,40 @@
 						<RenderMarkdown content={post.content} />
 					</div>
 				</div>
-				<!-- <footer class="">
-					<div
-						class="divide-gray-200 text-sm font-medium leading-5 dark:divide-gray-700 xl:col-start-1 xl:row-start-2 xl:divide-y"
-					>
-						{#if post.next || post.prev}
-							<div class="flex justify-between py-4 xl:block xl:space-y-8 xl:py-8">
-								{#if post.prev}
-									<div>
-										<h2 class="text-xs uppercase tracking-wide text-gray-500 dark:text-gray-400">
-											Previous Article
-										</h2>
-										<div
-											class="text-primary-500 hover:text-primary-600 dark:hover:text-primary-400"
-										>
-											<a href={base + `/blog/${post.prev.slug}`}>{post.prev.title}</a>
-										</div>
-									</div>
-								{/if}
-								{#if post.next}
-									<div>
-										<h2 class="text-xs uppercase tracking-wide text-gray-500 dark:text-gray-400">
-											Next Article
-										</h2>
-										<div
-											class="text-primary-500 hover:text-primary-600 dark:hover:text-primary-400"
-										>
-											<a href={base + `/blog/${post.next.slug}`}>{post.next.title}</a>
-										</div>
-									</div>
-								{/if}
-							</div>
-						{/if}
-					</div>
-				</footer> -->
 			</div>
+
+			<footer class="">
+				<div class="divide-gray-200 text-sm font-medium leading-5 dark:divide-gray-700">
+					{#if post.next || post.prev}
+						<h2 class="py-4 text-2xl font-bold">More Blog Posts</h2>
+
+						<div class="flex flex-row gap-4 py-4 xl:py-8">
+							{#if post.prev}
+								<div class="max-w-md flex-1">
+									<h2 class="text-xs uppercase tracking-wide text-gray-500 dark:text-gray-400">
+										Previous Article
+									</h2>
+									<div class="text-primary-500 hover:text-primary-600 dark:hover:text-primary-400">
+										<!-- <a href={base + `/blog/${post.prev.slug}`}>{post.prev.title}</a> -->
+										<BlogItem post={post.prev} />
+									</div>
+								</div>
+							{/if}
+							{#if post.next}
+								<div class="max-w-md flex-1">
+									<h2 class="text-xs uppercase tracking-wide text-gray-500 dark:text-gray-400">
+										Next Article
+									</h2>
+									<div class="text-primary-500 hover:text-primary-600 dark:hover:text-primary-400">
+										<!-- <a href={base + `/blog/${post.next.slug}`}>{post.next.title}</a> -->
+										<BlogItem post={post.next} />
+									</div>
+								</div>
+							{/if}
+						</div>
+					{/if}
+				</div>
+			</footer>
 		</div>
 	</article>
 </div>
