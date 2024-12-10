8aa05c32e43b8af5ddab2dfdee2fc61992210934
Kawsar Ahmed <prokawsar@gmail.com>
2024-12-10 20:28:40 +0600

    Top navigation bar (#16)

--- src/lib/components/CommunityMenus.svelte
+++ src/lib/components/CommunityMenus.svelte
@@ -1,8 +1,8 @@
 <script lang="ts">
 	import { base } from '$app/paths'
+	import CommunityMenuItem from '$lib/components/elements/CommunityMenuItem.svelte'
 	import { communityList } from '$utils/constants'
 	import Icon from '@iconify/svelte'
-	import CommunityMenuItem from '$lib/components/elements/CommunityMenuItem.svelte'
 
 	export let link
 </script>
@@ -10,23 +10,23 @@
 <div class="menus">
 	<a
 		href={base + link.href}
-		class="flex items-center p-1 font-medium text-white hover:text-arkOrange sm:p-2 dark:text-gray-100"
+		class="flex h-9 items-center gap-1 rounded-lg bg-white/10 p-1 px-4 font-medium text-arkGray7 hover:bg-arkOrangeLightM/10 hover:text-arkOrangeLightM dark:text-gray-100"
 	>
+		<Icon icon="fluent:people-community-28-filled" width="22px" />
 		{link.title}
-		<Icon icon="ic:outline-keyboard-arrow-down" width="24px" />
 	</a>
-	<div
-		class="apps-menu absolute top-10 grid w-96 grid-cols-2 rounded-xl border border-gray-300 bg-white p-4"
-	>
-		<div class="flex w-44 flex-col gap-3">
-			{#each communityList.slice(0, 4) as menu}
-				<CommunityMenuItem {menu} />
-			{/each}
-		</div>
-		<div class="flex w-40 flex-col gap-3">
-			{#each communityList.slice(4) as menu}
-				<CommunityMenuItem {menu} />
-			{/each}
+	<div class="apps-menu absolute top-9 pt-2">
+		<div class="grid w-96 grid-cols-2 rounded-xl border border-gray-300 bg-white p-4">
+			<div class="flex w-44 flex-col gap-3">
+				{#each communityList.slice(0, 4) as menu}
+					<CommunityMenuItem {menu} />
+				{/each}
+			</div>
+			<div class="flex w-40 flex-col gap-3">
+				{#each communityList.slice(4) as menu}
+					<CommunityMenuItem {menu} />
+				{/each}
+			</div>
 		</div>
 	</div>
 </div>
